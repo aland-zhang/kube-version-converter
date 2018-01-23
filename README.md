8cce22e47ad1c7fcc5f5e47399985c7d9e82254c
Vincent Cui <github@fleeto.us>
2018-01-23 09:50:49 +0800

    吃了没文化的亏，kubectl 自带转换命令。

--- README.md
+++ README.md
@@ -24,33 +24,9 @@ $ ./kube-ver-conv.py query DaemonSet
 1.9: apps/v1
 ```
 
-### Convert
-
-```
-usage: kube-ver-conv.py convert [-h] [-s [file name]] [-d [file name]] -t
-                                [output version] [-o [output format]]
-
-optional arguments:
--h, --help            show this help message and exit
--s [file name], --source-file [file name]
-                      YML or JSON file to process, '-' for STDIN.
--d [file name], --dest-file [file name]
-                      File name for output, '-' for STDOUT.
--t [output version], --to-version [output version]
-                      Kubernetes version of the out object file.
--o [output format], --output-format [output format]
-                      Format of the output file, default is yaml
-```
-
-**Example**
-
-`./kube-obj-ver-conv.py convert -s deploy.yaml  -d deploy.1.9.yaml -o yaml -t 1.9`
-
-
 ---
 
 # 中文版
-
 
 转换 Kuberntes API Object 到指定版本。
 
@@ -75,25 +51,3 @@ $ ./kube-ver-conv.py query DaemonSet
 
 1.9: apps/v1
 ```
-
-### 转换
-
-```
-用法: kube-ver-conv.py convert [-h] [-s [file name]] [-d [file name]] -t
-                                [output version] [-o [output format]]
-
-可选参数：
--h, --help            show this help message and exit
--s [file name], --source-file [file name]
-                      待处理的 JSON 或 YAML 文件，缺省为 “-”，代表从 STDIN 输入。
--d [file name], --dest-file [file name]
-                      输出文件名称，缺省为 “-”，代表输出到 STDOUT。
--t [output version], --to-version [output version]
-                      指定输出为 Kuberntes 版本的文件。
--o [output format], --output-format [output format]
-                      输出格式，可以指定 json 或者 yaml，缺省为 yaml
-```
-
-**例如**
-
-`./kube-obj-ver-conv.py convert -s deploy.yaml  -d deploy.1.9.yaml -o yaml -t 1.9`
--- kube-ver-conv.py
+++ kube-ver-conv.py
@@ -3,8 +3,6 @@
 import argparse
 import json
 import yaml
-import sys
-import io
 
 
 def record_to_string(record, version, with_header=True):
@@ -86,58 +84,8 @@ query_parser = subparsers.add_parser(
 query_parser.add_argument(
     "OBJECT", metavar="[Object Type]", choices=OBJECT_LIST)
 
-desc = '''
-Convert Kubernetes Objects to specified Kubernetes versions.
-'''
-convert_parser = subparsers.add_parser("convert", help=desc)
-convert_parser.add_argument("-s", "--source-file", metavar="[file name]",
-                            help="YML or JSON file to process, '-' for STDIN.",
-                            required=False, default="-")
-convert_parser.add_argument("-d", "--dest-file", metavar="[file name]",
-                            help="File name for output, '-' for STDOUT.",
-                            required=False, default="-")
-convert_parser.add_argument("-t", "--to-version", metavar="[output version]",
-                            help="Kubernetes version of \
-                            the out object file.",
-                            choices=K8S_VER, required=True)
-convert_parser.add_argument("-o", "--output-format",
-                            metavar="[output format]",
-                            help="Format of the output file, default is yaml",
-                            choices=FILE_FORMAT,
-                            required=False, default="yaml")
-
 args = vars(main_parser.parse_args())
-
-if "OBJECT" in args.keys():
-    obj = MATRIX[args["OBJECT"]]
-    for version in obj.keys():
-        record = obj[version]
-        print record_to_string(record, version)
-
-else:
-    if args["source_file"] != "-":
-        obj = file_to_object(args["source_file"])
-    else:
-        input_lines = sys.stdin.readlines()
-        input_lines = (''.join(input_lines)).decode('unicode-escape')
-        first_char = input_lines[:1]
-        if first_char in ["{", "[", "\""]:
-            obj = json.loads(input_lines)
-            if type(obj) is dict:
-                obj = [obj]
-        else:
-            obj = yaml.load_all(io.StringIO(input_lines))
-    result_content = convert_list(obj, args["to_version"])
-
-    if args["output_format"] == "yaml":
-        result_content = yaml.dump_all(
-            result_content, default_flow_style=False)
-    if args["output_format"] == "json":
-        result_content = json.dumps(result_content, indent=True)
-    if args["dest_file"] == "-":
-        print result_content
-    else:
-        file_name = args["dest_file"]
-        text_file = open(file_name, "w")
-        text_file.write(result_content)
-        text_file.close()
+obj = MATRIX[args["OBJECT"]]
+for version in obj.keys():
+    record = obj[version]
+    print record_to_string(record, version)
